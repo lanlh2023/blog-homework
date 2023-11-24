67cf85ced55561611a63682d72087780f13a5145
Le Hoang Lan <lanlh@zigexn-vn>
2023-11-24 08:35:36 +0700

    feature/homework-post-delete: change notification and display content

--- resources/views/admin/post/show.blade.php
+++ resources/views/admin/post/show.blade.php
@@ -12,6 +12,7 @@
 @section('content')
     <!--Container Main start-->
     <div class="col-12 col-xl-10 col-lg-9 col-md-9 content-table-wrap">
+        @include('partial.form.toast-message')
         @if (!empty($post))
             <div class="content-detail card shadow" id="content-detail">
                 <div class="card-header">
@@ -28,7 +29,7 @@
                                 <img src="{{ asset($subContent->image) }}" alt="" class="w-100">
                             </div>
                             <div class="m-2">
-                                {{ $subContent->content }}
+                                {!! $subContent->content !!}
                             </div>
                         @endforeach
                     </div>
@@ -42,19 +43,6 @@
                 delete
             </button>
             @include('partial.form.delete-confirmation', ['model' => $post])
-            <div role="alert" aria-live="assertive" aria-atomic="true" class="toast" data-autohide="false">
-                <div class="toast-header">
-                    <img src="..." class="rounded mr-2" alt="...">
-                    <strong class="mr-auto">Bootstrap</strong>
-                    <small>11 mins ago</small>
-                    <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="toast-body">
-                    Hello, world! This is a toast message.
-                </div>
-            </div>
         @else
             <div class="d-flex justify-content-center" style="font-size: 20px">
                 Post Not Found
