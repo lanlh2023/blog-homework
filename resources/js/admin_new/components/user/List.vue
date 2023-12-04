b8cb49a69ee54c0292705055c1ed344ea5a8b731
Le Hoang Lan <lanlh@zigexn-vn>
2023-12-04 09:25:48 +0700

    feature/homework-user-list-vue: upload feature list user

---
+++ resources/js/admin_new/components/user/List.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="col-12 col-xl-10 col-lg-9 col-md-9 content-table-wrap">
+        <div class="pagination-wrap d-flex justify-content-end px-4 pt-4">
+            <pagination  v-if="users.data && users.data.length > 0" :meta="users.meta" :path="path" @page-change="pageChange" />
+        </div>
+        <div class="table w-100 py-4">
+            <div class="content-table col-12">
+                <Table v-if="users.data && users.data.length > 0" :users="users.data" :path="path"></Table>
+                <div class="d-flex justify-content-center" style="font-size: 20px" v-else>
+                    No User Found
+                </div>
+            </div>
+            <div class="btn-group m-3">
+                <a :href="path + '/create'" class="btn btn-primary d-flex align-items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="1.25em" viewBox="0 0 448 512">
+                        <path
+                            d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
+                    </svg>
+                    Add
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { reactive } from 'vue';
+import Table from './Table.vue';
+
+export default {
+    name: 'UserList',
+    data() {
+        return {
+            users: reactive({}),
+        };
+    },
+    components: {
+        Table,
+    },
+    props: [
+        'path',
+    ],
+    mounted() {
+        this.getUserList();
+    },
+    computed: {
+    },
+    methods: {
+        async getUserList(page = 1) {
+            try {
+                const response = await this.axios.get('/api/user?page=' + page);
+                this.users = response.data;
+            } catch (error) {
+                console.log(error);
+            }
+        },
+        pageChange(page) {
+            this.getUserList(page);
+        }
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ resources/js/admin_new/components/user/Table.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="content-table col-12">
+        <table class="table table-striped table-hover table-bordered align-middle table-responsive">
+            <thead>
+                <tr>
+                    <th class="align-middle" scope="col" style="width: 5%"><label>ID</label></th>
+                    <th class="align-middle" scope="col" style="width: 20%"><label>User Name</label></th>
+                    <th class="align-middle" scope="col" style="width: 20%"><label>Avatatar</label></th>
+                    <th class="align-middle" scope="col" style="width: 20%"><label>Position Name</label></th>
+                    <th class="align-middle" scope="col" style="width: 10%"><label>Created Date</label></th>
+                    <th class="align-middle" scope="col" style="width: 10%"><label>Updated Date</label></th>
+                    <th class="align-middle" scope="col" style="width: 15%"><label>Action</label></th>
+                </tr>
+            </thead>
+            <tbody>
+                <User v-for="user in users" :key="user.id" :user="user" :path="path">
+                </User>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+import User from './User.vue'
+export default {
+    name: 'Table',
+
+    data() {
+        return {
+
+        };
+    },
+    props: {
+        users: {
+            type: Array,
+            required: true,
+        },
+        path: {
+            type: String,
+            required: true,
+        },
+    },
+    components: {
+        User,
+    },
+    mounted() {
+    },
+
+    methods: {
+
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
