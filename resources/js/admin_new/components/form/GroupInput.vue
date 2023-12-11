dbd5db259986a20663639fc36dcd603c07487996
Le Hoang Lan <lanlh@zigexn-vn>
2023-12-11 16:34:54 +0700

    feature/homework-user-delete-vue: change space for GroupInput.vue

--- resources/js/admin_new/components/form/GroupInput.vue
+++ resources/js/admin_new/components/form/GroupInput.vue
@@ -1,58 +1,58 @@
 <template>
-	<div class="mb-3">
-		<label :for="input.id" class="form-label">{{ input.label }}
-		</label>
-		<input
+    <div class="mb-3">
+        <label :for="input.id" class="form-label">{{ input.label }}
+        </label>
+        <input
             class="form-control"
             :type="input.type"
             :name="input.name"
             :id="input.id"
             :placeholder="input.placeholder"
-			:value="inputValue"
+            :value="inputValue"
             :data-content="input.label"
             @input="handldeChange"
             />
-			<div class="error-div" :class="`error ${input.name}`">
-			<label :id="`${input.name}-error`" class="error text-danger" :for="input.name">
-				{{ input.error }}
-			</label>
-		</div>
-	</div>
+            <div class="error-div" :class="`error ${input.name}`">
+            <label :id="`${input.name}-error`" class="error text-danger" :for="input.name">
+                {{ input.error }}
+            </label>
+        </div>
+    </div>
 </template>
 <script>
 export default {
-	name: 'GroupInput',
-	props: {
-		input: {
-			type: Object,
-			required: true,
-		},
-	},
-	data() {
-		return {};
-	},
-	computed: {
-		inputValue: {
-			get() {
-				return this.input.value;
-			},
-			set(value) {
-				this.$emit("setup-form", this.input.name, value);
-			},
-		},
-	},
-	mounted() {
+    name: 'GroupInput',
+    props: {
+        input: {
+            type: Object,
+            required: true,
+        },
+    },
+    data() {
+        return {};
+    },
+    computed: {
+        inputValue: {
+            get() {
+                return this.input.value;
+            },
+            set(value) {
+                this.$emit("setup-form", this.input.name, value);
+            },
+        },
+    },
+    mounted() {
 
-	},
-	methods: {
-		handldeChange($event) {
-			let value = $event.target.value;
-			if ($event.target.type == 'file') {
-				value = $event.target.files[0];
-			}
+    },
+    methods: {
+        handldeChange($event) {
+            let value = $event.target.value;
+            if ($event.target.type == 'file') {
+                value = $event.target.files[0];
+            }
             this.$emit('setup-form', $event.target.name, value);
-		}
-	},
+        }
+    },
 };
 </script>
 <style lang="scss" scoped></style>
