45f2009b0040d516eda9d5fba68ab9d029b52274
Le Hoang Lan <lanlh@zigexn-vn>
2023-12-22 09:49:58 +0700

    feature/category: add logo

--- resources/views/partial/header.blade.php
+++ resources/views/partial/header.blade.php
@@ -1,5 +1,8 @@
 <nav class="navbar navbar-expand-lg navbar-light py-5 mb-10 container d-flex justify-content-xl-between justify-content-lg-between justify-content-md-center justify-content-sm-center">
-  <a class="navbar-brand col-md-1 col-sm-10 text-center pl-0" href="{{ route('home') }}">BLOG</a>
+  <a class="navbar-brand col-md-1 col-sm-10 text-center pl-0" href="{{ route('home') }}">
+    <img src="{{ asset('images/logo/logomark.min.svg')}}" alt="">
+    <img src="{{ asset('images/logo/logotype.min.svg') }}" alt="">
+    </a>
   <div class="navbar d-flex justify-content-between align-items-center justify-content-md-between justify-content-sm-between justify-content-xl-between">
     <ul class="ml-lg-auto flex-1 nav flex-nowrap ml-lg-auto d-flex justify-content-end align-items-center h-100">
       <li class="nav-item active">
