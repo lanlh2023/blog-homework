f752257501c3eac0a45a5e3641b5537b01acf38f
Le Hoang Lan <lanlh@zigexn-vn>
2024-03-29 11:00:46 +0700

    test pint

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 
-class DashboardController extends     Controller
+class DashboardController extends      Controller
 {
     /**
      * Render screen dashboard
