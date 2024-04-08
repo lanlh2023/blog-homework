fb08c0cb47f50d5decd232b4abdbee6a3acc4ed7
Le Hoang Lan <lanlh@zigexn-vn>
2024-04-08 11:09:39 +0700

    test

--- app/Console/Commands/SendMailCron.php
+++ app/Console/Commands/SendMailCron.php
@@ -67,5 +67,6 @@ class SendMailCron extends Command
             }
         }
         Log::info('End '.$this->signature.' : '.now());
+        Log::info('End '.$this->signature.' : '.now());
     }
 }
